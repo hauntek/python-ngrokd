b74a965f3114b431a7b9d5c0170fcbebba6d5eb2
Bernardo Arocho <hauntek@hotmail.com>
2025-02-24 19:24:51 +0800

    整合msg.py 到 ngrokd.py
    msg.py 和 ngrokd.py 二合一 python-ngrokd.py

--- ngrokd.py
+++ ngrokd.py
@@ -6,7 +6,9 @@
 import socket
 import ssl
 import sys
+import json
 import time
+import struct
 import logging
 import threading
 
@@ -18,7 +20,104 @@ if not sys.version_info >= (3, 0):
 else:
     from queue import Queue
 
-from msg import *
+def AuthResp(ClientId = '', Version = '2', MmVersion = '1.7', Error = ''):
+    Payload = dict()
+    Payload['ClientId'] = ClientId
+    Payload['Version'] = Version
+    Payload['MmVersion'] = MmVersion
+    Payload['Error'] = Error
+    body = dict()
+    body['Type'] = 'AuthResp'
+    body['Payload'] = Payload
+    buffer = json.dumps(body)
+    return(buffer)
+
+def NewTunnel(ReqId = '', Url = '', Protocol = '', Error = ''):
+    Payload = dict()
+    Payload['ReqId'] = ReqId
+    Payload['Url'] = Url
+    Payload['Protocol'] = Protocol
+    Payload['Error'] = Error
+    body = dict()
+    body['Type'] = 'NewTunnel'
+    body['Payload'] = Payload
+    buffer = json.dumps(body)
+    return(buffer)
+
+def ReqProxy():
+    Payload = dict()
+    body = dict()
+    body['Type'] = 'ReqProxy'
+    body['Payload'] = Payload
+    buffer = json.dumps(body)
+    return(buffer)
+
+def StartProxy(Url, ClientAddr):
+    Payload = dict()
+    Payload['Url'] = Url
+    Payload['ClientAddr'] = ClientAddr
+    body = dict()
+    body['Type'] = 'StartProxy'
+    body['Payload'] = Payload
+    buffer = json.dumps(body)
+    return(buffer)
+
+def Pong():
+    Payload = dict()
+    body = dict()
+    body['Type'] = 'Pong'
+    body['Payload'] = Payload
+    buffer = json.dumps(body)
+    return(buffer)
+
+def lentobyte(len):
+    return struct.pack('<LL', len, 0)
+
+def sendbuf(sock, buf, isblock = False):
+    if isblock:
+        sock.setblocking(1)
+
+    buffer = buf
+    butlen = 0
+    while True:
+        sendlen = sock.send(buffer[butlen:butlen + 1024])
+        butlen += sendlen
+        if butlen == len(buffer):
+            break
+
+    if isblock:
+        sock.setblocking(0)
+
+def sendpack(sock, msg, isblock = False):
+    if isblock:
+        sock.setblocking(1)
+    sock.sendall(lentobyte(len(msg)))
+    sock.sendall(msg.encode('utf-8'))
+    if isblock:
+        sock.setblocking(0)
+
+def tolen(v):
+    if len(v) == 8:
+        return struct.unpack('<II', v)[0]
+    return 0
+
+def getRandChar(length):
+    import random
+    _chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz"
+    return ''.join(random.sample(_chars, length))
+
+def md5(s):
+    import hashlib
+    return hashlib.md5(s.encode('utf-8')).hexdigest().lower()
+
+def httphead(request):
+    header, data = request.split('\r\n\r\n', 1)
+    headers = dict()
+    for line in header.split('\r\n')[1:]:
+        key, val = line.split(': ', 1)
+        headers[key] = val
+
+    return headers
 
 class NgrokServiceFX:
     def __init__(self):
